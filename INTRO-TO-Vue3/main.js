0bf9934d30f41a80b3edbdf7c930e5c83cb4ca9f
yangkang <1910297447@qq.com>
2023-03-10 09:52:13 +0800

    vue 学习

--- INTRO-TO-Vue3/main.js
+++ INTRO-TO-Vue3/main.js
@@ -4,7 +4,9 @@ const app =Vue.createApp({
         return {
             product :'Socks',
             description:'A warm fuzzy pair of socks.',
-            
+            image:'./assets/images/socks_blue.jpg',
+            url:'https://cn.vuejs.org',
+            inStock:true
         }
     },
 })
